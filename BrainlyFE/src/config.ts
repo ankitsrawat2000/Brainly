b7062adb6c13f992fa0a2649349971887743f546
Ankit <ankitsrawat2000@gmail.com>
2025-03-02 02:27:09 +0530

    fix .env

--- BrainlyFE/src/config.ts
+++ BrainlyFE/src/config.ts
@@ -3,19 +3,12 @@
 
 // const LOCALFE_SERVER = "http://localhost:5173";
 // export const FRONTEND_URL = typeof LOCALFE_SERVER === 'undefined' ? process.env.NEXT_PUBLIC_FRONTEND_URL : LOCALFE_SERVER;
-import dotenv from 'dotenv';
-dotenv.config();
 
 
+export const BACKEND_URL = import.meta.env.NEXT_PUBLIC_BACKEND_URL;
+export const FRONTEND_URL = import.meta.env.NEXT_PUBLIC_FRONTEND_URL;
 
-console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
-
-if (!process.env.JWT_PASSWORD) {
-    throw new Error("MONGO_URL is not defined in the environment variables.");
-}
-
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
+console.log(BACKEND_URL);
 
 // export const BACKEND_URL = "http://localhost:3000";
 // export const FRONTEND_URL = "http://localhost:5173";
