75297b28b2db8ceac0b10d389bb5b2a09845340e
Ankit <ankitsrawat2000@gmail.com>
2025-03-01 17:32:09 +0530

    Change config.ts

--- BrainlyFE/src/config.ts
+++ BrainlyFE/src/config.ts
@@ -1,5 +1,8 @@
-const LOCALBE_SERVER = "http://localhost:3000";
-export const BACKEND_URL = typeof LOCALBE_SERVER === 'undefined' ? process.env.NEXT_PUBLIC_BACKEND_URL : LOCALBE_SERVER;
+// const LOCALBE_SERVER = "http://localhost:3000";
+// export const BACKEND_URL = typeof LOCALBE_SERVER === 'undefined' ? process.env.NEXT_PUBLIC_BACKEND_URL : LOCALBE_SERVER;
 
-const LOCALFE_SERVER = "http://localhost:5173";
-export const FRONTEND_URL = typeof LOCALFE_SERVER === 'undefined' ? process.env.NEXT_PUBLIC_FRONTEND_URL : LOCALFE_SERVER;
+// const LOCALFE_SERVER = "http://localhost:5173";
+// export const FRONTEND_URL = typeof LOCALFE_SERVER === 'undefined' ? process.env.NEXT_PUBLIC_FRONTEND_URL : LOCALFE_SERVER;
+
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
