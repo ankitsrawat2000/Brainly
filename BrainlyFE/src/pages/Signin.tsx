67f37d1ede593c990c3f1a8ce305e84f06f9e13e
Ankit <ankitsrawat2000@gmail.com>
2025-03-01 05:38:34 +0530

    password hided

--- BrainlyFE/src/pages/Signin.tsx
+++ BrainlyFE/src/pages/Signin.tsx
@@ -37,8 +37,8 @@ export function Signin(){
         <div className="h-screen w-screen bg-gray-200 flex
         justify-center items-center">
             <div className="bg-white rounded-xl border min-w-48 p-8">
-                <Input reference={usernameRef} placeholder="Username"/>
-                <Input reference={passwordRef} placeholder="Password"/>
+                <Input reference={usernameRef} placeholder="Username" type="text"/>
+                <Input reference={passwordRef} placeholder="Password" type="password"/>
                 <div className="flex justify-center pt-4">
                     <Button onClick={signin} variant="primary" text="Signin" fullWidth={true} loading={false}/>
                 </div>
--- BrainlyFE/src/pages/Signup.tsx
+++ BrainlyFE/src/pages/Signup.tsx
@@ -35,8 +35,8 @@ export function Signup(){
         <div className="h-screen w-screen bg-gray-200 flex
         justify-center items-center">
             <div className="bg-white rounded-xl border min-w-48 p-8">
-                <Input reference={usernameRef} placeholder="Username"/>
-                <Input reference={passwordRef} placeholder="Password"/>
+                <Input reference={usernameRef} placeholder="Username" type="text"/>
+                <Input reference={passwordRef} placeholder="Password" type="password"/>
                 <div className="flex justify-center pt-4">
                     <Button onClick={signup} variant="primary" text="Signup" fullWidth={true} loading={false}/>
                 </div>
